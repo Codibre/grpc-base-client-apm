b89272878fdf4e0aa612128f66c721cebea2a836
Thiago Santos <tos.oliveira@gmail.com>
2023-02-23 22:33:27 -0300

    feat: apm grpc client instrumentation

--- src/grpc-apm-middleware.ts
+++ src/grpc-apm-middleware.ts
@@ -1,13 +1,14 @@
 import { DefaultGrpcMiddleware } from 'grpc-base-client';
 import * as apm from 'elastic-apm-node';
+import { ServerDuplexStream } from '@grpc/grpc-js';
 
-export const apmClientMiddleware: DefaultGrpcMiddleware =
-	function apmMiddleware([metadata], method: string) {
-		const { currentTransaction } = apm;
-		if (currentTransaction) {
-			const { ids } = currentTransaction;
-			metadata.add('grpc-apm-middleware-trace.id', ids['trace.id']);
-			metadata.add('grpc-apm-middleware-transaction.id', ids['transaction.id']);
+const TRACE_PARENT_ID = 'elastic-apm-traceparent';
+
+export const gprcApmClientMiddleware: DefaultGrpcMiddleware =
+	function gprcApmClientMiddleware([metadata], method: string) {
+		const { currentTraceparent } = apm;
+		if (currentTraceparent) {
+			metadata.add(TRACE_PARENT_ID, currentTraceparent);
 			const span = apm.startSpan(`grpc call ${method}`);
 
 			return span
@@ -20,3 +21,23 @@ export const apmClientMiddleware: DefaultGrpcMiddleware =
 				: undefined;
 		}
 	};
+
+export function grpcServerUnaryWrapper<P, R>(
+	stream: ServerDuplexStream<P, R>,
+	method: string,
+) {
+	const { metadata } = stream;
+	const [transactionId] = metadata.get(TRACE_PARENT_ID);
+	const span = apm.startTransaction(`grpc call ${method}`, {
+		childOf: transactionId?.toString(),
+	});
+
+	return span
+		? {
+				onEnd: (error: unknown) => {
+					span.setOutcome(error ? 'failure' : 'success');
+					span.end();
+				},
+		  }
+		: undefined;
+}
